31d1c13bf9c3abe1c44f90088bd631da10d7ea39
davquipe <davidcondori99@gmail.com>
2022-06-06 15:47:09 -0500

    Nav & Footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,25 @@
-import { Box } from "@chakra-ui/react";
-import Head from "next/head";
+import Navbar from './Navbar';
+import { Box } from '@chakra-ui/react';
+import Head from 'next/head';
+import Footer from './Footer';
 
 const Layout = ({ children }) => (
     <>
         <Head>
             <title>Real Estate</title>
         </Head>
-        <Box maxWidth='1280px' m='auto'>
+        <Box maxWidth="1280px" m="auto">
             <header>
-                Navbar
+                <Navbar />
             </header>
             <main>
                 { children }
             </main>
             <footer>
-                Footer
+                <Footer />
             </footer>
         </Box>
     </>
-)
+);
 
-export default Layout
+export default Layout;
