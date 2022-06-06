77962653b3f9bd1b7bc761ec5217453c9948a60c
davquipe <davidcondori99@gmail.com>
2022-06-06 17:44:46 -0500

    Search Page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next/image'
+
+const SearchFilters = () => {
+    const [first, setfirst] = useState(false)
+  return (
+    <div>
+        SearchFilters
+    </div>
+  )
+}
+
+export default SearchFilters
